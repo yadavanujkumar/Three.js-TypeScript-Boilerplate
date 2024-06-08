d6fdeccd7621b4473eb7494d771bc8328c543280
yadavanujkumar <thunderstruck9999@gmail.com>
2024-06-08 13:58:47 +0530

    6624

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'dat.gui'
-
 
 const scene = new THREE.Scene()
 
@@ -31,23 +29,11 @@ scene.add(cube)
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const gui = new GUI()
-
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 20)
-cameraFolder.open()
-
 function animate() {
   requestAnimationFrame(animate)
 
-//  cube.rotation.x += 0.01
- // cube.rotation.y += 0.01
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
 
   renderer.render(scene, camera)
 
