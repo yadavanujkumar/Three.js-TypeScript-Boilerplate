7d5abe3b11c946f599ea7af26694f7c61d0945b6
yadavanujkumar <thunderstruck9999@gmail.com>
2024-06-19 00:06:49 +0530

    walkin

--- src/main.ts
+++ src/main.ts
@@ -37,100 +37,100 @@ controls.target.set(0, 0.75, 0)
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-// function lerp(from: number, to: number, speed: number) {
-//   const amount = (1 - speed) * from + speed * to
-//   return Math.abs(from - to) < 0.001 ? to : amount
-// }
+function lerp(from: number, to: number, speed: number) {
+  const amount = (1 - speed) * from + speed * to
+  return Math.abs(from - to) < 0.001 ? to : amount
+}
 
-//let mixer: THREE.AnimationMixer
-//let animationActions: { [key: string]: THREE.AnimationAction } = {}
-//let activeAction: THREE.AnimationAction
-// let speed = 0,
-//   toSpeed = 0
+let mixer: THREE.AnimationMixer
+let animationActions: { [key: string]: THREE.AnimationAction } = {}
+let activeAction: THREE.AnimationAction
+let speed = 0,
+  toSpeed = 0
 
 new GLTFLoader().load('models/eve$@walk.glb', (gltf) => {
-  //   mixer = new THREE.AnimationMixer(gltf.scene)
-  //   //console.log(gltf  )
+  mixer = new THREE.AnimationMixer(gltf.scene)
+    // console.log(gltf  )
 
-  //   mixer.clipAction(gltf.animations[0]).play()
+   mixer.clipAction(gltf.animations[0]).play()
 
-  scene.add(gltf.scene)
+ // scene.add(gltf.scene)
 })
 
-// async function loadEve() {
-//   const loader = new GLTFLoader()
-//   const [eve, idle, run] = await Promise.all([
-//     loader.loadAsync('models/eve$@walk.glb'),
-//     loader.loadAsync('models/eve@idle.glb'),
-//     loader.loadAsync('models/eve@run.glb')
-//   ])
+async function loadEve() {
+  const loader = new GLTFLoader()
+  const [eve, idle, run] = await Promise.all([
+    loader.loadAsync('models/eve$@walk.glb'),
+    loader.loadAsync('models/eve@idle.glb'),
+    loader.loadAsync('models/eve@run.glb')
+  ])
 
-//   mixer = new THREE.AnimationMixer(eve.scene)
+  mixer = new THREE.AnimationMixer(eve.scene)
 
-//   // mixer.clipAction(idle.animations[0]).play()
+  mixer.clipAction(idle.animations[0]).play()
 
-//   // animationActions['idle'] = mixer.clipAction(idle.animations[0])
-//   // animationActions['walk'] = mixer.clipAction(eve.animations[0])
-//   // animationActions['run'] = mixer.clipAction(run.animations[0])
+   animationActions['idle'] = mixer.clipAction(idle.animations[0])
+   animationActions['walk'] = mixer.clipAction(eve.animations[0])
+   animationActions['run'] = mixer.clipAction(run.animations[0])
 
-//   // animationActions['idle'].play()
-//   // activeAction = animationActions['idle']
+   animationActions['idle'].play()
+   activeAction = animationActions['idle']
 
-//   scene.add(eve.scene)
-// }
-// await loadEve()
+  scene.add(eve.scene)
+}
+await loadEve()
 
-// const keyMap: { [key: string]: boolean } = {}
+const keyMap: { [key: string]: boolean } = {}
 
-// const onDocumentKey = (e: KeyboardEvent) => {
-//   keyMap[e.code] = e.type === 'keydown'
-// }
-// document.addEventListener('keydown', onDocumentKey, false)
-// document.addEventListener('keyup', onDocumentKey, false)
+const onDocumentKey = (e: KeyboardEvent) => {
+  keyMap[e.code] = e.type === 'keydown'
+}
+document.addEventListener('keydown', onDocumentKey, false)
+document.addEventListener('keyup', onDocumentKey, false)
 
-// const clock = new THREE.Clock()
-// let delta = 0
+ const clock = new THREE.Clock()
+ let delta = 0
 
 function animate() {
   requestAnimationFrame(animate)
 
-  //delta = clock.getDelta()
+  delta = clock.getDelta()
 
   controls.update()
 
-  //mixer.update(delta)
+  mixer.update(delta)
 
-  // if (keyMap['KeyW']) {
-  //   if (keyMap['ShiftLeft']) {
-  //     //run
-  //     if (activeAction != animationActions['run']) {
-  //       activeAction.fadeOut(0.5)
-  //       animationActions['run'].reset().fadeIn(0.25).play()
-  //       activeAction = animationActions['run']
-  //       // toSpeed = 4
-  //     }
-  //   } else {
-  //     //walk
-  //     if (activeAction != animationActions['walk']) {
-  //       activeAction.fadeOut(0.5)
-  //       animationActions['walk'].reset().fadeIn(0.25).play()
-  //       activeAction = animationActions['walk']
-  //       // toSpeed = 1
-  //     }
-  //   }
-  // } else {
-  //   //idle
-  //   if (activeAction != animationActions['idle']) {
-  //     activeAction.fadeOut(0.5)
-  //     animationActions['idle'].reset().fadeIn(0.25).play()
-  //     activeAction = animationActions['idle']
-  //     // toSpeed = 0
-  //   }
-  // }
+  if (keyMap['KeyW']) {
+    if (keyMap['ShiftLeft']) {
+      //run
+      if (activeAction != animationActions['run']) {
+        activeAction.fadeOut(0.5)
+        animationActions['run'].reset().fadeIn(0.25).play()
+        activeAction = animationActions['run']
+        // toSpeed = 4
+      }
+    } else {
+      //walk
+      if (activeAction != animationActions['walk']) {
+        activeAction.fadeOut(0.5)
+        animationActions['walk'].reset().fadeIn(0.25).play()
+        activeAction = animationActions['walk']
+        // toSpeed = 1
+      }
+    }
+  } else {
+    //idle
+    if (activeAction != animationActions['idle']) {
+      activeAction.fadeOut(0.5)
+      animationActions['idle'].reset().fadeIn(0.25).play()
+      activeAction = animationActions['idle']
+      // toSpeed = 0
+    }
+  }
 
-  // speed = lerp(speed, toSpeed, delta * 10)
-  // gridHelper.position.z -= speed * delta
-  // gridHelper.position.z = gridHelper.position.z % 10
+  speed = lerp(speed, toSpeed, delta * 10)
+  gridHelper.position.z -= speed * delta
+  gridHelper.position.z = gridHelper.position.z % 10
 
   renderer.render(scene, camera)
 
